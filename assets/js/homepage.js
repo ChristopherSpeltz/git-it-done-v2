8771753fe0b8f8c19224562e9c555baef2f59eaf
Christopher Speltz <chrisspeltz@msn.com>
2023-07-05 18:55:24 -0500

    6.1.6

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,13 @@
-var getUsersRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos").then(function (response) {
-    console.log("inside", response);
+var getUsersRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
     });
   });
-
-  console.log("outside");
 };
 
-getUsersRepos();
+getUsersRepos("facebook");
