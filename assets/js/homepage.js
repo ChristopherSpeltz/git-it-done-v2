a70a0fdcfbeedaaa0d80f2b991c453d4a9636262
Christopher Speltz <chrisspeltz@msn.com>
2023-07-06 22:56:06 -0500

    6.2.4

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUsersRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,4 +13,19 @@ var getUsersRepos = function (user) {
   });
 };
 
-getUsersRepos("facebook");
+// function to be executed upon a form submission browser event
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  console.log(event);
+//   get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+    getUsersRepos(username)
+    nameInputEl.value = ""
+} else {
+    alert("Please enter a GitHub username")
+}
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
