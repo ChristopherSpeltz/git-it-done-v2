a07d49355b6b93a491e5ef377b28802f058eb8f2
Christopher Speltz <chrisspeltz@msn.com>
2023-07-08 20:43:19 -0500

    6.4.5

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,16 +3,25 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function () {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  console.log(repoName);
-  getRepoIssues(repoName);
-  repoNameEl.textContent = repoName;
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
 };
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+  //   make a get request to url
   fetch(apiUrl).then(function (response) {
     // request was successful
     if (response.ok) {
@@ -22,15 +31,15 @@ var getRepoIssues = function (repo) {
 
         // check if api has paginated issues
         if (response.headers.get("Link")) {
-          //   console.log("repo has more than 30 issues");
           displayWarning(repo);
         }
       });
     } else {
-      alert("There was a problem with your request!");
+        // if not successful, rediredt to homepage
+        document.location.replace("./index.html")
+    //   alert("There was a problem with your request!");
     }
   });
-  //   console.log(repo);
 };
 
 var displayIssues = function (issues) {
